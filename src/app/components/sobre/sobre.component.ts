02b767aa9f62c7f0a38540356bdb88936938f6d6
Marco Junior <60262274+marcojrdev@users.noreply.github.com>
2024-08-27 15:46:59 -0300

    add

--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -12,10 +12,11 @@ import { Component, Input } from '@angular/core';
 })
 export class SobreComponent {
   sobres = [
-    {name: "Wilton Araujo", cargo: "UX/UI Designer", photoUrl: 'assets/pessoal/Wilton-Araujo.jpg', like: "https://www.linkedin.com/in/marco-junior-307513216/" },
+    {name: "Wilton Araujo", cargo: "UX/UI Designer", photoUrl: 'assets/pessoal/Wilton-Araujo.jpg', like: "https://www.linkedin.com/in/wilton-araujo-31a197249/" },
     {name: "Marco Junior", cargo: "Dev Full Stack", photoUrl: 'assets/pessoal/Marco-Junior.png', like: "https://www.linkedin.com/in/marco-junior-307513216/" },
-    {name: "Lourival Barboza", cargo: "Co Fundador", photoUrl: 'assets/pessoal/Lourival-Barboza.jpg', like: "https://www.linkedin.com/in/marco-junior-307513216/" },
-    {name: "Rafael Cavalcante", cargo: "Gerente", photoUrl: 'assets/pessoal/Rafael-Cavalcante.jpg', like: "https://www.linkedin.com/in/marco-junior-307513216/" }
+    {name: "Lourival Barboza", cargo: "Co Fundador", photoUrl: 'assets/pessoal/Lourival-Barboza.jpg', like: "http://www.linkedin.com/in/lourival-barboza-8b75031b6/" },
+    {name: "Rafael Cavalcante", cargo: "Gerente", photoUrl: 'assets/pessoal/Rafael-Cavalcante.jpg', like: "https://www.linkedin.com/in/rafael-cavalcante-71a485176/" },
+    {name: "Palhares", cargo: "Front-End", photoUrl: 'assets/pessoal/Palhares.jpg', like: "#" }
   ];
 
 }
